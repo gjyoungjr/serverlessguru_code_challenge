89d131ff427ca629252b1176972f338810f24195
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2022-11-24 22:40:15 -0600

    organized files and added comments

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 npm install -g serverless
-serverless deploy --stage $env --package \   $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
+serverless deploy --stage $STAGE_NAME --package \   $CODEBUILD_SRC_DIR/target/$STAGE_NAME -v -r us-east-1
